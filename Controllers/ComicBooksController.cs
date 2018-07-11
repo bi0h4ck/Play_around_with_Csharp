2eca4ab651f342f14eae55e4df506807f51c8866
bi0h4ck <juliediempham@gmail.com>
2018-07-11 15:09:40 -0500

    Add ComicBooksController

---
+++ Controllers/ComicBooksController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public ActionResult Detail()
+        {
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Wednesday)
+            {
+                return new RedirectResult("/");
+            }
+            return Content("Hello world!!!!");
+        }
+    }
+}
